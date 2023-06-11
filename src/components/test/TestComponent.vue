41500fbdfee89acb178d9d5a1b97cd23a8b1c1f8
minhducctchv <sieunhanbay1997@gmail.com>
2023-06-11 17:34:18 +0700

    format code

--- src/components/test/TestComponent.vue
+++ src/components/test/TestComponent.vue
@@ -5,15 +5,15 @@
 </template>
 
 <script setup>
-    import { ref, computed, unref } from 'vue'
+import { ref, computed, unref } from 'vue'
 
-    const bien1 = ref('giatri1')
-    const bien2 = ref('giatri2')
-    const bien3 = ref('giatri3')
+const bien1 = ref('giatri1')
+const bien2 = ref('giatri2')
+const bien3 = ref('giatri3')
 
-    const computedName = computed(() => {
-        return unref(bien1) + unref(bien2) + unref(bien3)
-    })
+const computedName = computed(() => {
+    return unref(bien1) + unref(bien2) + unref(bien3)
+})
 </script>
 
 <style lang="scss" scoped></style>
